b74061e1bc2a3e5e108c80efb878a6e2661545d7
Eric Bartos <djbartos93@gmail.com>
2018-08-10 23:52:37 -0500

    Create lazy-docker.sh

---
+++ lazy-docker.sh
@@ -0,0 +1,8 @@
+sudo yum check-update
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl status docker
+sudo systemctl enable docker
+sudo usermod -aG docker $(whoami)
+docker info
+docker run hello-world
